8e5269ad388328bed31660d2b722b5cee57ffa15
Niranjan Shrestha <z@192.168.0.53>
2023-06-15 21:47:40 -0400

    auto_get.sh: creating sh

---
+++ auto_get.sh
@@ -0,0 +1,10 @@
+GIT_REPO_URL='https://github.com/niranjanshr13/home-automation-tasker-helper'
+REMOTE_REPO_HEAD=$(git ls-remote $GIT_REPO_URL HEAD | awk {'print $1'})
+GIT_REPO_PATH='/data/data/com.termux/files/home/storage/shared/.automation/home-automation-tasker-helper'
+LOCAL_REPO_HEAD=$(git ls-remote $GIT_REPO_PATH HEAD | head -1 | awk {'print $1'})
+
+if [[ $REMOTE_REPO_HEAD != $LOCAL_REPO_HEAD ]]
+then
+rm -rf $GIT_REPO_PATH
+git clone $GIT_REPO_URL $GIT_REPO_PATH
+fi
