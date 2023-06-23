7b5861f70a61414eebb6805e8cc0e0c32372632f
Niranjan Shrestha <niranjanshr13@users.noreply.github.com>
2023-06-22 20:27:52 -0400

    Update auto_get.sh

--- auto_get.sh
+++ auto_get.sh
@@ -5,6 +5,11 @@ LOCAL_REPO_HEAD=$(git ls-remote $GIT_REPO_PATH HEAD | head -1 | awk {'print $1'}
 
 if [[ $REMOTE_REPO_HEAD != $LOCAL_REPO_HEAD ]]
 then
+# refresh the git repo.
 rm -rf $GIT_REPO_PATH
 git clone $GIT_REPO_URL $GIT_REPO_PATH
+# ssh config
+
+# setuping bashrc
+
 fi
