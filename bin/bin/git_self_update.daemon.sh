529366bced96be31ba3a3660add5e8986efc39e8
Niranjan Shrestha <niranjanshr13@users.noreply.github.com>
2023-07-05 21:38:16 -0400

    .

--- bin/bin/git_self_update.daemon.sh
+++ bin/bin/git_self_update.daemon.sh
@@ -6,7 +6,7 @@ GIT_SSH_COMMAND="ssh -i $GIT_SSH_PATH"
 GIT_REPO_URL='git@github.com:niranjanshr13/android-termux'
 
 REMOTE_REPO_HEAD=$(git ls-remote $GIT_REPO_URL HEAD | awk {'print $1'})
-GIT_REPO_PATH='$HOME/'
+GIT_REPO_PATH='$HOME/android-termux'
 LOCAL_REPO_HEAD=$(git ls-remote $GIT_REPO_PATH HEAD | head -1 | awk {'print $1'})
 
 if [[ $REMOTE_REPO_HEAD != $LOCAL_REPO_HEAD ]]
