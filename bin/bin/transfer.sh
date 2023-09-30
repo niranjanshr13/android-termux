efab7386142bbad248b93bfba6123a759c25a8fb
Niranjan Shrestha <niranjanshr13@users.noreply.github.com>
2023-09-30 17:19:37 -0400

    cleaning stuff

--- bin/bin/transfer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-TRANSFER_PATH="$HOME/storage/shared/.automation"
-rm -rfv $TRANSFER_PATH
-
-mkdir $TRANSFER_PATH
-mv $HOME/android-termux/bin/data/* $TRANSFER_PATH
-rm -f $HOME/android-termux/bin/bin/transfer.sh
