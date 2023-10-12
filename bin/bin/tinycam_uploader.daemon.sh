37f11a85915a2aad146fba962beecfa4c375169d
Niranjan Shrestha <niranjanshr13@users.noreply.github.com>
2023-10-12 00:28:40 -0400

    test: cleaning phase

--- bin/bin/tinycam_uploader.daemon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-RCLONE_CONFIG_PATH="$HOME/custom/config/rclone.conf"
-BACKUP_PATH="$HOME/storage/shared/tinycammon/Recordings/"
-while :
-do
-rclone --config $RCLONE_CONFIG_PATH --include *sec*.mp4 move $BACKUP_PATH tinycam:cams/
-sleep 10
-done
